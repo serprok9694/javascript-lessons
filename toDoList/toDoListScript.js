c0f467d67469796fece3599b750609dd83a5fb58
serprok9694 <serprok9694@mail.ru>
2021-09-02 21:37:59 +0300

    updated toDoList js

--- toDoList/toDoListScript.js
+++ toDoList/toDoListScript.js
@@ -1,6 +1,21 @@
+window.addEventListener('load', () => {
+    const tasks = JSON.parse(localStorage.getItem('toDoTasks'));
+    if (tasks.length) {
+        const tasksElements = tasks.map(task => `
+            <div class="task">
+                <span id="taskname">${task}</span>
+                <button class="delete">Delete</button>
+            </div>
+        `);
+        document.querySelector('#tasks').innerHTML = tasksElements.join("");
+    };
+});
+
 const createNewTask = function () {
     const inputValue = document.querySelector('#newTask input').value;
     if (inputValue) {
+        const toDoTasks = JSON.parse(localStorage.getItem('toDoTasks')) || [];
+        localStorage.setItem('toDoTasks', JSON.stringify([...toDoTasks, inputValue]));
         document.querySelector("#tasks").innerHTML += `
             <div class="task">
                 <span id="taskname">${inputValue}</span>
